f4b23ec1b4e8bf67f7e0c52fabacded8c546a05a
Enmanuel Jarquin <enmisac@gmail.com>
2020-09-26 11:21:26 -0600

    add mongo config

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 const { ApolloServer } = require("apollo-server");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
 const typeDefs = require("./typeDefs");
 const resolvers = require("./importResolvers");
+
+mongoose.connect(`${process.env.DB_HOST}/${process.env.DB_NAME}`);
 
 const server = new ApolloServer({
   typeDefs,
