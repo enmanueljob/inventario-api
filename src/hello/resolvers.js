f9bacefdd130eca0d0d18a79e7699e1ff03a07b0
Orlando Altamirano <oaltamirano@ni.grouppbs.net>
2020-10-03 10:33:47 -0600

    Creacion de Esquema de Cliente, Input y Query

--- src/hello/resolvers.js
+++ src/hello/resolvers.js
@@ -1,5 +1,23 @@
+let data = [
+  {
+  id:1,
+  name: "Pedro",
+  telefono: "88888888",
+  email:"pedro@gmail.com",
+  },
+  {
+  id:2,
+  name: "Pablo",
+  telefono: 77777777,
+  email:"pablo@gmail.com",
+  },
+  ]
+
 module.exports = {
   Query: {
     hello: () => "Hello world!",
+    clients: () => data
   },
 };
+
+
